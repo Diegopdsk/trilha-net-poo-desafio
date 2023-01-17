17d5ba64ead200cf8fcc10ffc07d6a3b2f598f23
Diego Pereira <diegomusico.rcc@gmail.com>
2023-01-16 21:29:06 -0300

    Desafio de POO em C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Informações do Celular Nokia");
+Nokia nokia = new Nokia("985223236", "1100", "12345632", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhattsApp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("________________________________________________________");
+
+Console.WriteLine("Informações do Celular Iphone");
+Iphone iphone = new Iphone("999175485", "6S", "33366985", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
